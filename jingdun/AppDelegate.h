7b9308613b4ad28542237707065338660e7a1b2d
mayingbing <mayingbing@163.com>
2016-06-12 15:04:08 +0800

    jingdun
    jingdun

---
+++ jingdun/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  jingdun
+//
+//  Created by 马颖兵 on 16/6/12.
+//  Copyright © 2016年 yingwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ jingdunTests/jingdunTests.m
@@ -0,0 +1,39 @@
+//
+//  jingdunTests.m
+//  jingdunTests
+//
+//  Created by 马颖兵 on 16/6/12.
+//  Copyright © 2016年 yingwan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface jingdunTests : XCTestCase
+
+@end
+
+@implementation jingdunTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
